efe8afd3f562f07a4a4c0ea26d83a8843d2b7044
Neroxgd <darkaux69@gmail.com>
2023-01-23 10:02:36 +0100

    gravity fixed

--- Assets/Aurélien/Scripts/PlayerController.cs
+++ Assets/Aurélien/Scripts/PlayerController.cs
@@ -9,14 +9,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody rbPlayer;
     [SerializeField] private Transform maxLeftXPos, maxRightXPos, maxUpYPos, maxDownYPos;
     [SerializeField] private float Powergravity = 9.81f;
+    [SerializeField] private Camera cam;
+    private bool isOnRotate = false;
+    private bool IsOnGround = false;
     private float gravity = 0;
 
     public void Move(bool direction)
     {
-        if (direction)
-            rbPlayer.DOMove(ClampPosPlayer(transform.right), speedMove);
-        else
-            rbPlayer.DOMove(ClampPosPlayer(-transform.right), speedMove);
+        if (IsOnGround)
+        {
+            if (direction)
+                rbPlayer.DOMove(ClampPosPlayer(transform.right * 1), speedMove);
+            else
+                rbPlayer.DOMove(ClampPosPlayer(-transform.right * 1), speedMove);
+        }
     }
 
     private Vector3 ClampPosPlayer(Vector3 dir)
@@ -28,12 +34,27 @@ public class PlayerController : MonoBehaviour
 
     public void ChangeGravity(int directionGravity)
     {
-        if (directionGravity == 0)
-            transform.DOLocalRotate(new Vector3(0, 0, -180), speedRotate, RotateMode.LocalAxisAdd);
-        else if (directionGravity == -1)
-            transform.DOLocalRotate(new Vector3(0, 0, -90), speedRotate, RotateMode.LocalAxisAdd);
-        else if (directionGravity == 1)
-            transform.DOLocalRotate(new Vector3(0, 0, 90), speedRotate, RotateMode.LocalAxisAdd);
+        if (IsOnGround)
+        {
+            StartCoroutine(IsOnRotate());
+            if (directionGravity == 0)
+            {
+                transform.DOLocalRotate(new Vector3(0, 0, -180), speedRotate, RotateMode.LocalAxisAdd);
+                cam.transform.DOLocalRotate(new Vector3(0, 0, -180), speedRotate, RotateMode.LocalAxisAdd);
+            }
+
+            else if (directionGravity == -1)
+            {
+                transform.DOLocalRotate(new Vector3(0, 0, -90), speedRotate, RotateMode.LocalAxisAdd);
+                cam.transform.DOLocalRotate(new Vector3(0, 0, -90), speedRotate, RotateMode.LocalAxisAdd);
+            }
+            else if (directionGravity == 1)
+            {
+                transform.DOLocalRotate(new Vector3(0, 0, 90), speedRotate, RotateMode.LocalAxisAdd);
+                cam.transform.DOLocalRotate(new Vector3(0, 0, 90), speedRotate, RotateMode.LocalAxisAdd);
+            }
+        }
+
     }
 
     void FixedUpdate()
@@ -43,14 +64,27 @@ public class PlayerController : MonoBehaviour
 
     private void Gravity()
     {
+        Ray ray = new Ray(transform.position, -transform.up);
         gravity += Powergravity * Time.fixedDeltaTime;
         rbPlayer.velocity += -transform.up * gravity * Time.fixedDeltaTime;
-        Ray ray = new Ray(transform.position, -transform.up);
+
         Debug.DrawRay(transform.position, -transform.up * 0.55f, Color.green);
         if (Physics.Raycast(ray, 0.55f))
         {
+            IsOnGround = true;
             gravity = 1;
             print("sol");
         }
+        else
+            IsOnGround = false;
+    }
+
+    IEnumerator IsOnRotate()
+    {
+        isOnRotate = true;
+        rbPlayer.constraints = RigidbodyConstraints.FreezePosition;
+        yield return new WaitForSeconds(speedRotate);
+        rbPlayer.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+        isOnRotate = false;
     }
 }
