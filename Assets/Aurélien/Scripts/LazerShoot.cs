1c0c94227030e47f468e25c60a161a4051ab17c0
Neroxgd <darkaux69@gmail.com>
2023-02-18 02:41:42 +0100

    lazer effect

--- Assets/Aurélien/Scripts/LazerShoot.cs
+++ Assets/Aurélien/Scripts/LazerShoot.cs
@@ -9,14 +9,20 @@ public class LazerShoot : MonoBehaviour
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private float speedLazer = 1;
     [SerializeField] private Ease easeLazer;
+    [SerializeField] private AnimationCurve shakeLazerX;
+    [SerializeField] private AnimationCurve shakeLazerY;
+
     private List<Transform> listLazer;
     public void ShootLazer()
     {
         AudioManager.Instance.PlaySound(lazerSound);
         GameObject currentLazer = Instantiate(lazer, spawnPoint.position, Quaternion.identity);
-        currentLazer.transform.DOMove(currentLazer.transform.forward * 50, speedLazer)
+        currentLazer.transform.DOMoveZ(currentLazer.transform.forward.z * 30, speedLazer)
         .SetSpeedBased(true)
         .SetEase(easeLazer)
         .OnComplete(() => Destroy(currentLazer));
+        currentLazer.transform.DOMoveX(currentLazer.transform.right.x * 30, speedLazer).SetEase(shakeLazerX).SetSpeedBased(true);
+        currentLazer.transform.DOMoveY(currentLazer.transform.up.y * 30, speedLazer).SetEase(shakeLazerY).SetSpeedBased(true);
+
     }
 }
